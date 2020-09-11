1e17d8f6df98c19ef84bb73b6d26a631f811fe14
Tony Zhou <hanyongz@student.unimelb.edu.au>
2020-09-12 00:00:08 +0800

    Added collision and borders

--- Assets/Scripts/FreeCam.cs
+++ Assets/Scripts/FreeCam.cs
@@ -4,40 +4,39 @@ using UnityEngine;
 
 public class FreeCam : MonoBehaviour
 {
-    public float speed = 10f;
-    public Camera cam;
-    public float rotateSpeed = 2.5f;
+    public float speed = 25f;
+    public float terrainSize = 128f;
+    public float startHeight = 40f;
+    public CharacterController controller;
+    public float rotateSpeed = 1f;
     private float xRotation;
     private float yRotation;
-    // Start is called before the first frame update
+    private Camera cam;
     void Start()
     {
         cam = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
+        cam.transform.position = new Vector3(terrainSize/2, startHeight, terrainSize/2);
+        cam.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKey(KeyCode.A)) {
-            cam.transform.position = cam.transform.position + (-1) * cam.transform.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            cam.transform.position = cam.transform.position + cam.transform.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            cam.transform.position = cam.transform.position + cam.transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            cam.transform.position = cam.transform.position + (-1) * cam.transform.forward * speed * Time.deltaTime;
-        }
-
+    {   
+        // Movement of camera on its relative x and z axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = cam.transform.right*x+cam.transform.forward*z;
+        controller.Move(move*speed*Time.deltaTime);
+        
+        // Rotation of camera
         xRotation += Input.GetAxis("Mouse X") * rotateSpeed;
         yRotation += Input.GetAxis("Mouse Y") * rotateSpeed;
         cam.transform.rotation = Quaternion.Euler(-yRotation, xRotation, 0);
-    }
 
-    void OnCollisionEnter () {
-        print("Hit");
+        if (Input.GetKeyDown("space")) {
+            cam.transform.position = new Vector3(terrainSize/2, startHeight, terrainSize/2);
+            cam.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
     }
 }
